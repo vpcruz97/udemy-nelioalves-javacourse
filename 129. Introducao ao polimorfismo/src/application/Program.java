b0ab8c04f85fc1a41c099a4a276ace0a1a4dc19f
Vitor Pereira da Cruz <vitor-pcruz@hotmail.com>
2021-03-11 11:15:36 -0300

    Aula 129. Introducao ao Polimorfismo(ja aplicado em outras aulas)

---
+++ 129. Introducao ao polimorfismo/src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Uso do polimorfismo ja fora aplicado em demais aulas
+		Account x = new Account(1020, "Alex", 1000.0);
+		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+		
+		x.withdraw(50.0);
+		y.withdraw(50.0);
+		
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
+	}
+}
