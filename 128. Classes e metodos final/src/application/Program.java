efcd5f4a9b736f79714f2430b3f02eb498467afd
Vitor Pereira da Cruz <vitor-pcruz@hotmail.com>
2021-03-11 11:04:54 -0300

    128. classe e metodo final (nada utilizado dentro da classe Program)

---
+++ 128. Classes e metodos final/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		// Anotacoes nas classes
+		
+	}
+}
