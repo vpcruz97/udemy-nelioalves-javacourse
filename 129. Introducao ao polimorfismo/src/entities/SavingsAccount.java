96cb86415c5fda5a1f95f7f8bdfb1414552076d9
Vitor Cruz <vitor-pcruz@hotmail.com>
2021-04-04 20:58:17 -0300

    Aula 132. Classes Abstrata adcionada

--- 129. Introducao ao polimorfismo/src/entities/SavingsAccount.java
+++ 129. Introducao ao polimorfismo/src/entities/SavingsAccount.java
@@ -21,9 +21,9 @@ public class SavingsAccount extends Account {
 	public void updateBalance() {
 		balance += balance * interestRate;
 	}
-	
 	@Override
 	public void withdraw(double amount) {
 		balance -= amount;
 	}
+	
 }
