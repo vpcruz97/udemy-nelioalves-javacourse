61e2134c93e22046b363ca7ea9fea41b13446216
Vitor Cruz <vitor-pcruz@hotmail.com>
2021-04-05 17:56:51 -0300

    Aplicacao de try-with-resources na aula 183

--- 183. FileReader e BufferedReader/src/application/Program.java
+++ 183. FileReader e BufferedReader/src/application/Program.java
@@ -8,12 +8,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		String path = "..//in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr); // Abstração maior do FileReader
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			
 			String line = br.readLine();
 			
@@ -25,19 +21,7 @@ public class Program {
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
-		finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				
-				if (fr != null) {
-					fr.close();
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		
 	}
 }
+
