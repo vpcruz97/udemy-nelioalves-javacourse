1e2bb7d82de17267799154fb05dc3c6378c8a130
Vitor Cruz <vitor-pcruz@hotmail.com>
2021-04-06 16:16:08 -0300

    Aula 198 adicionada

---
+++ 204. Introducao aos Generics/src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		PrintService ps = new PrintService();
+		
+		System.out.print("How many values? ");
+		int n = sc.hasNextInt();
+		
+		for (int i = 0;)
+	}
+}
