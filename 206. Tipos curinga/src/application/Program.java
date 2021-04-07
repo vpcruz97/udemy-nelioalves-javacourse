146c4137470dd7961930dc881144ac6ba5ec90a4
Vitor Cruz <vitor-pcruz@hotmail.com>
2021-04-07 10:51:57 -0300

    Aula 206 adicionada. Tipos Curinga

---
+++ 206. Tipos curinga/src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.*;
+
+public class Program {
+
+	public static void main(String[] args) {
+//		List<?> myObjs = new ArrayList<Object>();
+//		List<Integer> myNumbers = new ArrayList<Integer>();
+//		myObjs = myNumbers;
+//		
+//		Object obj;
+//		Integer x = 10;
+//		obj = x;
+		
+		
+//		List<Integer> myInts = Arrays.asList(5,2,10);
+//		printList(myInts);
+//		
+//		List<String> myStrs = Arrays.asList("Maria", "Alex", "Bob");
+//		printList(myStrs);
+//	}
+//	
+//	public static void printList(List<?> list) {
+//		for (Object obj: list) {
+//			System.out.println(obj);
+//		}
+			
+	}
+}
